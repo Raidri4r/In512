fb857f268f0a3fbe07275067f0a5d54bac451473
Piceltiqua <salome.delpierre@ipsa.fr>
2025-12-10 17:04:31 +0100

    Initial commit, movement but no avoidance or anything

--- scripts/agent.py
+++ scripts/agent.py
@@ -11,22 +11,26 @@ from threading import Thread
 import numpy as np
 from time import sleep
 
+DIRECTION = DOWN_RIGHT
+DIAGONAL = (-1, 1)
 
 class Agent:
     """ Class that implements the behaviour of each agent based on their perception and communication with other agents """
     def __init__(self, server_ip):
-        #TODO: DEINE YOUR ATTRIBUTES HERE
-        self.my_key = () #store coordinates of the agent key
-        self.my_chest = () #same for the chest
-        self.mission_completed = 0 #Nb of agent that have got their key and reach the chest to know when the game is done
-        self.map = None
-        self.direction = None 
-        self.vertical_direction = 'down'
-        self.min_border_distance = 2
-        self.vertical_step = 5 
-        self.state = 'init'
-        self.vertical_moves_remaining = 0
-        self.cell_val = 0  
+        self.my_key = ()
+        self.my_chest = ()
+        self.total_items_found = 0
+        self.mission_completed = 0 # Nb of agent that have their key and reached the chest
+        self.status = INIT
+        self.direction = DIAGONAL
+        self.start_pos = {}
+        self.target = (0,0)
+        self.next_target = None
+
+        self.direction = DOWN_LEFT 
+        self.step = 9
+        
+        self.steps_remaining = 0
         self.waiting_for_move = False
 
         #DO NOT TOUCH THE FOLLOWING INSTRUCTIONS
@@ -40,11 +44,11 @@ class Agent:
         self.nb_agent_connected = 0
         self.x, self.y = env_conf["x"], env_conf["y"]   #initial agent position
         self.w, self.h = env_conf["w"], env_conf["h"]   #environment dimensions
-        cell_val = env_conf["cell_val"] #value of the cell the agent is located in
-        print(cell_val)
+        self.map = np.zeros((self.w, self.h))
+        self.cell_val = env_conf["cell_val"] #value of the cell the agent is located in
         Thread(target=self.msg_cb, daemon=True).start()
-        print("hello")
         self.wait_for_connected_agent()
+        self.start_pos[self.agent_id] = (self.x, self.y)
 
         
     def msg_cb(self): 
@@ -54,35 +58,42 @@ class Agent:
             self.msg = msg
             if msg["header"] == MOVE:
                 self.x, self.y =  msg["x"], msg["y"]
+                self.map[self.x, self.y] = msg["cell_val"]
                 self.cell_val = msg["cell_val"]
-                self.waiting_for_move = False  
+                self.waiting_for_move = False
+
             elif msg["header"] == GET_NB_AGENTS:
                 self.nb_agent_expected = msg["nb_agents"]
+
             elif msg["header"] == GET_NB_CONNECTED_AGENTS:
                 self.nb_agent_connected = msg["nb_connected_agents"]
+
             elif msg["header"] == BROADCAST_MSG:
                 if msg['owner'] == self.agent_id:
-                    if msg['Msg type'] == KEY_DISCOVERED:
+                    if msg['type'] == KEY_DISCOVERED:
                         self.my_key = msg['position']
                         print(f"Key of agent {self.agent_id} was found in {msg['position']}")
-                    elif msg['Msg type'] == BOX_DISCOVERED:
+
+                    elif msg['type'] == BOX_DISCOVERED:
                         self.my_chest = msg['position']
                         print(f"Chest of agent {self.agent_id} was found in {msg['position']}")
-                elif msg['Msg type'] == 3:
+
+                elif msg['type'] == 3:
                     self.mission_completed +=1
+
+                elif msg['type'] == START_POSITION:
+                    self.start_pos[msg['owner']] = (msg['x'], msg['y'])
+
             elif msg['header'] == GET_DATA:
                 self.map = np.zeros((msg['h'], msg['w']))
-                
+
             elif msg['header'] == GET_ITEM_OWNER:
                 if msg['owner']:
                     cmds = {"header": 0}
-                    cmds["Msg type"] = 1 if self.state == 'on_key' else 2
+                    cmds["type"] = 1 if self.state == 'on_key' else 2
                     cmds["position"] = (agent.x, agent.y)
                     cmds["owner"] = msg['owner']
                     agent.network.send(cmds)
-
-
-            print(f"Received by {self.agent_id}: {msg}")
       
             
 
@@ -93,7 +104,7 @@ class Agent:
             self.network.send({"header": GET_NB_CONNECTED_AGENTS})
             sleep(0.5)
             if self.nb_agent_expected and self.nb_agent_expected == self.nb_agent_connected:
-                print("both connected!")
+                print("All agents connected")
                 check_conn_agent = False
 
                   
@@ -161,7 +172,7 @@ class Agent:
                         self.network.send({"header": MOVE, "direction": direction})
                         sleep(1)
 
-    def get_move_direction(self, dx, dy):
+    def get_direction(self, dx, dy):
         """"Return the msg type for the move command"""
 
         if dx == -1 and dy == 0:
@@ -181,156 +192,151 @@ class Agent:
         elif dx == 1 and dy == 1:
             return DOWN_RIGHT
         return STAND
-
-    def move_to_initial_position(self):
-        """"Move the agent to an appropriate initial position 3 cell away from edge"""
-        target_x = self.x
-        target_y = self.y
-
-        if self.x < self.min_border_distance:
-            target_x = self.min_border_distance
-        elif self.x > self.w - 1 - self.min_border_distance:
-            target_x = self.w - 1 - self.min_border_distance
-
-        if self.y < self.min_border_distance:
-            target_y = self.min_border_distance
-        elif self.y > self.h - 1 - self.min_border_distance:
-            target_y = self.h - 1 - self.min_border_distance
-
-        if self.x == target_x and self.y == target_y:
-            return False  
-
-        dx = 0
-        dy = 0
-
-        if self.x < target_x:
-            dx = 1
-        elif self.x > target_x:
-            dx = -1
-
-        if self.y < target_y:
-            dy = 1
-        elif self.y > target_y:
-            dy = -1
-
-        direction = self.get_move_direction(dx, dy)
+    
+    def move(self, dx, dy):
+        """"Move the agent in the specified direction"""
+        direction = self.get_direction(dx, dy)
         cmds = {"header": MOVE, "direction": direction}
         self.network.send(cmds)
         self.waiting_for_move = True
 
-        return True
+    def move_towards(self, tx: int, ty: int) -> bool:
+        """"Move the agent towards its target.
+        Return True if the target is reached"""
 
-    def determine_initial_direction(self):
-        """"Determine the initial direction that the agent will follow to explore"""
-        center_x = self.w / 2
-        center_y = self.h / 2
+        dx = tx - self.x
+        dy = ty - self.y
 
-        if self.x <= center_x:
-            self.direction = 'right'
-        else:
-            self.direction = 'left'
+        # Return True if we are already at the target
+        if dx == 0 and dy == 0:
+            return True
 
-        if self.y <= center_y:
-            self.vertical_direction = 'down'
-        else:
-            self.vertical_direction = 'up'
+        dx = int(dx > 0) - int(dx < 0)
+        dy = int(dy > 0) - int(dy < 0)
 
-        print(f"Agent {self.agent_id} - Direction: {self.direction}, Vertical: {self.vertical_direction}")
+        self.move(dx, dy)
 
-    def is_near_horizontal_border(self):
-        """"Chekc if agent is close to border to change direction"""
-        if self.direction == 'right':
-            return self.x >= self.w - 1 - self.min_border_distance
-        else:
-            return self.x <= self.min_border_distance
+        return False
 
-    def is_near_vertical_border(self):
         """"Chekc if agent is close to border to change direction"""
         if self.vertical_direction == 'down':
             return self.y >= self.h - 1 - self.min_border_distance
         else:
             return self.y <= self.min_border_distance
 
+    def divide_map(self):
+        """ Divide the map into nb_agent_expected regions and return the target position and direction for the agent """
+        match self.nb_agent_expected:
+            case 2:
+                start = [(2, 2), (self.w -3, 2)]
+                
+            case 3:
+                if self.w >= self.h:
+                    d = int(np.sqrt((self.w - self.h) * self.h + self.h**2/2))
+                    start = [(2, 2), (d, 2), (self.h - d, self.w - 3)]
+                
+                else:
+                    d = int(np.sqrt((self.h - self.w) * self.w + self.w**2/2))
+                    start = [(2, 2), (2, d), (self.h -3, self.w - d)]
+
+            case 4:
+                if self.w >= self.h:
+                    d = int(np.sqrt(((self.w - self.h) * self.h + self.h**2)/2))
+                    start = [(2, 2), (self.w - d, 2), (2, self.h -3), (self.w -3, self.h - d)]
+
+                else:
+                    d = int(np.sqrt(((self.h - self.w) * self.w + self.w**2)/2))
+                    start = [(2, 2), (self.h - d, 2), (2, self.w -3), (self.h -3, self.w - d)]
+
+
+            case _:
+                pass
+
+        for pos in start:
+            min = float('inf')
+            min_id = -1
+            for agent_id, agent_pos in self.start_pos.items():
+                dist = np.sqrt((pos[0] - agent_pos[0])**2 + (pos[1] - agent_pos[1])**2)
+                if dist < min:
+                    min = dist
+                    min_id = agent_id
+        
+            if min_id == self.agent_id:
+                return pos[0], pos[1]
+            else:
+                self.start_pos.pop(min_id)
+                
+        
     def run(self):
 
-        if self.waiting_for_move: #Wait for server response
+        if self.waiting_for_move: 
+            return # Wait for server response
+        
+        if self.status == INIT:
+            self.network.send({"header": BROADCAST_MSG, "type": START_POSITION ,"owner": self.agent_id, "x": self.x, "y": self.y})
+            if len(self.start_pos) == self.nb_agent_expected:
+                self.target = self.divide_map()
+                print(f"Agent {self.agent_id} - Target position: {self.target}")
+                self.status = TOSTART
             return
+        
+        # if self.cell_val == KEY_NEIGHBOUR_PERCENTAGE/2:
+        #     self.status = FIND_OBJECT
 
-        if self.cell_val != 0:
-            # Handle BOX detection
-            if self.cell_val == BOX_NEIGHBOUR_PERCENTAGE/2:
-                self.state = 'close_to_box'
-                self.explore_towards_target(
-                    close_threshold=BOX_NEIGHBOUR_PERCENTAGE/2,
-                    very_close_threshold=BOX_NEIGHBOUR_PERCENTAGE,
-                    target_value=1,
-                    close_state='close_to_box',
-                    very_close_state='very_close_to_box',
-                    on_target_state='on_box'
-                )
-                return
+        # if self.cell_val == BOX_NEIGHBOUR_PERCENTAGE/2:
+        #     self.status = FIND_OBJECT
 
-            # Handle KEY detection
-            elif self.cell_val == KEY_NEIGHBOUR_PERCENTAGE/2:
-                self.state = 'close_to_key'
-                self.explore_towards_target(
-                    close_threshold=KEY_NEIGHBOUR_PERCENTAGE/2,
-                    very_close_threshold=KEY_NEIGHBOUR_PERCENTAGE,
-                    target_value=1,
-                    close_state='close_to_key',
-                    very_close_state='very_close_to_key',
-                    on_target_state='on_key'
-                )
-                return
+        # if self.cell_val == WALL_NEIGHBOUR_PERCENTAGE:
+        #     self.status = AVOID_WALL
 
 
-                
-
-
-        if self.state == 'init':
-            if self.move_to_initial_position():
-                return  
-
-            self.determine_initial_direction()
-            self.state = 'horizontal'
-            print(f"Agent {self.agent_id} - Position initiale atteinte: ({self.x}, {self.y})")
+        if self.status == TOSTART:
+            if self.move_towards(self.target[0], self.target[1]):
+                self.status = EXPLORING
+                self.direction = LEFT
             return
+        
+        if self.status == EXPLORING:
+            print(f"Agent {self.agent_id} - Exploring towards {self.target} in direction {self.direction}")
+            if self.move_towards(self.target[0], self.target[1]):
 
-        if self.state == 'horizontal':
+                if self.next_target is not None:
+                    self.target = self.next_target
+                    self.next_target = None
 
-            if self.is_near_horizontal_border():
-                self.direction = 'left' if self.direction == 'right' else 'right'
+                elif self.direction == DOWN_LEFT:
+                    vertical_move = min(9, (self.h - 2) - self.y)
+                    horizontal_move = 9 - vertical_move
 
-                if self.is_near_vertical_border():
-                    self.vertical_direction = 'up' if self.vertical_direction == 'down' else 'down'
+                    if  0 < vertical_move < 9:
+                        self.target = (self.x, self.y + vertical_move)
+                        self.next_target = (self.x + horizontal_move, self.y + vertical_move)
+                    else:
+                        self.target = (self.x + horizontal_move, self.y + vertical_move)
 
-                self.vertical_moves_remaining = self.vertical_step
-                self.state = 'vertical'
-                return
+                    self.direction = DOWN
 
-            dx = 1 if self.direction == 'right' else -1
-            direction = self.get_move_direction(dx, 0)
-            cmds = {"header": MOVE, "direction": direction}
-            self.network.send(cmds)
-            self.waiting_for_move = True
+                elif self.direction == DOWN:
+                    dist = min(self.y - 2, self.w - 3 - self.x)
+                    self.target = (self.x + dist, self.y - dist)
+                    self.direction = UP_RIGHT
+
+                elif self.direction == UP_RIGHT:
+                    horizontal_move = min(9, (self.w - 2) - self.x)
+                    vertical_move = 9 - horizontal_move
+
+                    if 0 < horizontal_move < 9:
+                        self.target = (self.x + horizontal_move, self.y)
+                        self.next_target = (self.x + horizontal_move, self.y + vertical_move)
+                    else:
+                        self.target = (self.x + horizontal_move, self.y + vertical_move)
+                    self.direction = LEFT
+
+                elif self.direction == LEFT:
+                    dist = min(self.x - 2, self.h - 3 -self.y)
+                    self.target = (self.x - dist, self.y + dist)
+                    self.direction = DOWN_LEFT
             return
-
-        if self.state == 'vertical':
-            if self.vertical_moves_remaining <= 0:
-                self.state = 'horizontal'
-                return
-
-            if self.is_near_vertical_border():
-                self.vertical_direction = 'up' if self.vertical_direction == 'down' else 'down'
-                self.state = 'horizontal'
-                return
-
-            dy = 1 if self.vertical_direction == 'down' else -1
-            direction = self.get_move_direction(0, dy)
-            cmds = {"header": MOVE, "direction": direction}
-            self.network.send(cmds)
-            self.waiting_for_move = True
-            self.vertical_moves_remaining -= 1
 
  
 if __name__ == "__main__":
--- scripts/my_constants.py
+++ scripts/my_constants.py
@@ -4,6 +4,8 @@ __credits__ = ["Aybuke Ozturk Suri", "Johvany Gustave"]
 __license__ = "Apache License 2.0"
 __version__ = "1.0.0"
 
+import numpy as np
+
 """ This file contains all the 'constants' shared by all the scripts """
 
 """ MSG HEADERS """
@@ -26,6 +28,7 @@ DOWN_LEFT = 7
 DOWN_RIGHT = 8
 
 """ BROADCAST TYPES """
+START_POSITION = 0
 KEY_DISCOVERED = 1  #inform other agents that you discovered a key
 BOX_DISCOVERED = 2
 COMPLETED = 3   #inform other agents that you discovered your key and you reached your own box
@@ -37,9 +40,40 @@ BOX_NEIGHBOUR_PERCENTAGE = 0.6  #value of an adjacent cell to a key
 KEY_TYPE = 0    #one of the types of item that is output by the 'Get item owner' request
 BOX_TYPE = 1
 WALL_NEIGHBOUR_PERCENTAGE = 0.35
+
 """ GUI """
 BG_COLOR = (255, 255, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+
+""" STATUS """
+INIT = 1
+TOSTART = 2
+EXPLORING = 3
+FIND_OBJECT = 4
+AVOID_WALL = 5
+TO_KEY = 6
+TO_BOX = 7
+COMPLETED = 8
+
+""" OBJECTS"""
+
+KEY = np.array([[0.25, 0.25, 0.25, 0.25, 0.25],
+                [0.25, 0.50, 0.50, 0.50, 0.25],
+                [0.25, 0.50, 1.00, 0.50, 0.25],
+                [0.25, 0.50, 0.50, 0.50, 0.25],
+                [0.25, 0.25, 0.25, 0.25, 0.25]])
+
+BOX = np.array([[0.30, 0.30, 0.30, 0.30, 0.30],
+                [0.30, 0.60, 0.60, 0.60, 0.30],
+                [0.30, 0.60, 1.00, 0.60, 0.30],
+                [0.30, 0.60, 0.60, 0.60, 0.30],
+                [0.30, 0.30, 0.30, 0.30, 0.30]])
+
+WALL = np.array([[0.35, 0.35, 0.35, 0.00, 0.00],
+                 [0.35, 1.00, 0.35, 0.00, 0.00],
+                 [0.35, 1.00, 0.35, 0.35, 0.35],
+                 [0.35, 1.00, 1.00, 1.00, 0.35],
+                 [0.35, 0.35, 0.35, 0.35, 0.35]])
